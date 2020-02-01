8d32868114461a6fe137bd0d4b75832f8066690a
john <hszhongtao@163.com>
2020-02-01 22:06:57 +0800

    session设置

--- am/settings.py
+++ am/settings.py
@@ -26,6 +26,32 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+# redis在django中的配置
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://192.168.0.103:6379",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "CONNECTION_POOL_KWARGS": {"max_connections": 100},
+            "PASSWORD": "test123",
+        }
+    }
+}
+
+# session 设置
+SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'  # 引擎
+SESSION_CACHE_ALIAS = 'default'  # 使用的缓存别名（默认内存缓存，也可以是memcache），此处别名依赖缓存的设置
+
+SESSION_COOKIE_NAME = "amid"  # Session的cookie保存在浏览器上时的key,即:amid＝随机字符串
+SESSION_COOKIE_PATH = "/"  # Session的cookie保存的路径
+SESSION_COOKIE_DOMAIN = None  # Session的cookie保存的域名
+SESSION_COOKIE_SECURE = False  # 是否Https传输cookie
+SESSION_COOKIE_HTTPONLY = True  # 是否Session的cookie只支持http传输
+SESSION_COOKIE_AGE = 1209600  # Session的cookie失效日期（2周）
+SESSION_EXPIRE_AT_BROWSER_CLOSE = False  # 是否关闭浏览器使得Session过期
+SESSION_SAVE_EVERY_REQUEST = False  # 是否每次请求都保存Session，默认修改之后才保存
+
 # Application definition
 
 INSTALLED_APPS = [
